e6be0af993480603c0e5d20bb2ff0ba9670b80a0
Oscar <Oscar@DIMA>
2026-07-11 16:56:49 +0300

    Fastify init

--- nest-app/src/main.ts
+++ nest-app/src/main.ts
@@ -1,12 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import {
+    FastifyAdapter,
+    NestFastifyApplication,
+} from '@nestjs/platform-fastify';
 
-async function bootstrap() {
-    const app = await NestFactory.create(AppModule);
+async function bootstrap(): Promise<void> {
+    const app = await NestFactory.create<NestFastifyApplication>(
+        AppModule,
+        new FastifyAdapter(),
+    );
     await app.listen(process.env.PORT ?? 3000);
 }
 
-bootstrap().catch((err) => {
+bootstrap().catch((err: unknown) => {
     console.error('Error starting the application:', err);
     process.exit(1);
 });
